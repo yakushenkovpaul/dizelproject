d89db05c0c7d34afd94cc8be5d486165ac899e98
pavelandreev <yakushenkovpaul@gmail.com>
2020-02-24 10:20:20 +0300

    add

---
+++ app/Http/Controllers/Tarantool.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Tarantool\Client\Client;
+use Tarantool\Client\Schema\Criteria;
+
+use Illuminate\Http\Request;
+
+class Tarantool extends Controller
+{
+	public function index() {
+
+		$client = Client::fromOptions([
+			'uri' => ENV('TARANTOOL_URL'),
+			'username' => ENV('TARANTOOL_USER'),
+			'password' => ENV('TARANTOOL_PASSWORD')
+		]
+	);
+
+		$space = $client->getSpace(ENV('TARANTOOL_SPACE'));
+		$result = $space->select(Criteria::key([1]));
+
+		print('<pre>');
+		print_r($result);
+
+ }
+}
